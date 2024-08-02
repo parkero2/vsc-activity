f5e49051b9ea9ab7dd33ea366b9a3b879005abdb
parkero2 <72895391+parkero2@users.noreply.github.com>
2024-08-03 11:01:45 +1200

    Socket server implimentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,67 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Server, Socket } from "socket.io";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const PORT: number = 5000;
+var isServerRunning: boolean = false;
+const io: Server = new Server(PORT);
+
+interface EditorStatus {
+	fileName: string;
+	workspaceFolder: string;
+}
+
+var editorStatus: EditorStatus = {
+	fileName: 'None',
+	workspaceFolder: 'None'
+};
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsc-activity" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vsc-activity.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VSC_ACTIVITY!');
+	io.on('connection', (socket: Socket) => {
+		console.log('Client connected');
+		socket.emit('editorStatus', editorStatus);
 	});
 
-	context.subscriptions.push(disposable);
+	const serverStart = vscode.commands.registerCommand('vsc-activity.StartServer', () => {
+		vscode.window.showInformationMessage(`Starting socket server on port ${PORT}.`);
+		if (isServerRunning) { return vscode.window.showInformationMessage(`Server is already running on port ${PORT}.`); }
+		try {
+			io.listen(PORT);
+			isServerRunning = true;
+			vscode.window.showInformationMessage(`Server started on port ${PORT}.`);
+		} catch (error) {
+			vscode.window.showErrorMessage(`Failed to start server on port: ${PORT}.`);
+			isServerRunning = false;
+		}
+
+	});
+
+	const serverStop = vscode.commands.registerCommand('vsc-activity.StopServer', () => {
+		vscode.window.showInformationMessage(`Stopping socket server on port ${PORT}`);
+		if (!isServerRunning) { return vscode.window.showInformationMessage(`Server is already not running on port ${PORT}`); }
+		try {
+			io.close();
+			isServerRunning = false;
+			vscode.window.showInformationMessage(`Server stopped on port ${PORT}.`);
+		} catch (error) {
+			vscode.window.showErrorMessage(`Failed to stop server on port: ${PORT}.`);
+			isServerRunning = true;
+		}
+	});
+
+	const fileChange = vscode.window.onDidChangeActiveTextEditor((editor) => {
+		if (!isServerRunning) { return; }
+		if (!editor) { return; }
+		editorStatus.fileName = editor.document.fileName || 'None';
+		editorStatus.workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri)?.name || 'None';
+		io.emit('editorStatus', editorStatus);
+	});
+
+
+	context.subscriptions.push(serverStart);
+	context.subscriptions.push(serverStop);
+	context.subscriptions.push(fileChange);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
